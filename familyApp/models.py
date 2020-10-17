9967319696ac0433a8f0c930eddd2fbfc891c20a
SHorne41 <stephane.horne@gmail.com>
2020-10-17 17:12:46 -0400

    Custom User model implemented

---
+++ familyApp/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+import datetime
+
+class CustomUser(AbstractUser):
+    pass
+
+class CalendarEvent(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    owner = models.ForeignKey("CustomUser", on_delete=models.CASCADE)
+    members = models.ManyToManyField("CustomUser", related_name="event_members")
+
+class Calendar(models.Model):
+    owner = models.ForeignKey("CustomUser", on_delete=models.CASCADE)
+    events = models.ForeignKey("CalendarEvent", on_delete=models.PROTECT)
+
+class TodoList(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey("CustomUser", on_delete=models.CASCADE)
+    members = models.ManyToManyField("CustomUser", related_name="list_members")
+
+class TodoEvent(models.Model):
+    name = models.CharField(max_length=100)
+    dealine = models.DateTimeField()
+    owner = models.ForeignKey("CustomUser", on_delete=models.CASCADE)
+    list = models.ForeignKey("TodoList", on_delete=models.CASCADE)
+
+class Message(models.Model):
+    sender = models.ForeignKey("CustomUser", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    content = models.TextField(max_length=480)
