2f6cd658eb0e0622d583b71bfae540d7b811c8cf
SHorne41 <stephane.horne@gmail.com>
2020-10-18 17:38:02 -0400

    Login page prompts users for email and password. Register link brings users to registration page (should be a pop-up). Layout/styling still needs to be addressed

--- familyApp/views.py
+++ familyApp/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def loginView(request):
-    return render(request, "familyApp/login.html")
+    if request.method == "POST":
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("calendar"))
+        else:
+            return render(request, "familyApp/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "familyApp/login.html")
 
 def registerView(request):
     return render(request, "familyApp/register.html")
 
 def calendarView(request):
-
     return render(request, "familyApp/calendar.html")
