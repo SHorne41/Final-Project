47afae334c36776aaef560c95d7311e096efbb4a
SHorne41 <stephane.horne@gmail.com>
2020-10-18 17:18:03 -0400

    Adding login/registration files. Functionality not yet implemented

--- familyApp/views.py
+++ familyApp/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def loginView(request):
+    return render(request, "familyApp/login.html")
+
+def registerView(request):
+    return render(request, "familyApp/register.html")
+
 def calendarView(request):
 
     return render(request, "familyApp/calendar.html")
