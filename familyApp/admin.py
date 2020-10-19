77ebc7c7c97e2c5303ca5cf9f96c59b966a0bbb6
SHorne41 <stephane.horne@gmail.com>
2020-10-18 20:05:48 -0400

    Made custom fields visible in admin

--- familyApp/admin.py
+++ familyApp/admin.py
@@ -10,5 +10,6 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username',]
+    fieldsets = (*UserAdmin.fieldsets, ('Custom fieldsets', {'fields': ('role', 'group', 'calendarEmail', 'profile_pic'),},),)
 
 admin.site.register(CustomUser, CustomUserAdmin)
