51301d27117003bc19084787514fde374166d73d
SHorne41 <stephane.horne@gmail.com>
2020-10-25 11:53:25 -0400

    successfully converted links to new pages into single page app using JS. views.py needs to be cleaned up accordingly

--- familyApp/static/familyApp/script.js
+++ familyApp/static/familyApp/script.js
@@ -1,28 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     //Add event listeners for navigation pane buttons
-    document.querySelector('#profile_view').addEventListener('click', () => load_view('profile'));
+    document.querySelector('#profile_tab').addEventListener('click', () => load_view('profile'));
     document.querySelector('#calendar_view').addEventListener('click', () => load_view('calendar'));
     document.querySelector('#chores_view').addEventListener('click', () => load_view('chores'));
     document.querySelector('#chat_view').addEventListener('click', () => load_view('chat'));
     document.querySelector('#sync_feature').addEventListener('click', () => sync_calendar());
 
     //Load the calendar view by default
-    load_view('calendar');
+    load_view('profile');
 
 });
 
 function load_view(view){
     //Hide all content-areas. Then, using if block, display the one we need
-    document.querySelectorAll("[data-content]").style.display='none';
+    let divs = document.querySelectorAll("[data-content]");
+    for (i = 0; i < divs.length; i ++){
+        divs[i].style.display = 'none';
+    }
 
-    if (view == 'profile'){
-        document.querySelector("[data-content='profile']").style.display = 'block';
-    } elseif (view == 'calendar'){
-        document.querySelector("[data-content='calendar']").style.display = 'block';
-    } elseif (view == 'chores'){
+    if (view === 'profile') {
+        let profileDiv = document.querySelector("[data-content='profile']")
+        profileDiv.style.display = 'block';
+        profileDiv.innerHTML = '';
+        let success = document.createElement('p');
+        success.innerHTML = "Success bitch";
+        profileDiv.appendChild(success);
+
+    } else if (view === 'calendar'){
+        let calendarDiv = document.querySelector("[data-content='calendar']")
+        calendarDiv.style.display = 'block';
+        calendarDiv.innerHTML = '';
+        let success = document.createElement('p');
+        success.innerHTML = "Calendar Success bitch";
+        calendarDiv.appendChild(success);
+    } else if (view === 'chores'){
         document.querySelector("[data-content='chores']").style.display = 'block';
-    } elseif (view == 'chat'){
+    } else if (view === 'chat'){
         document.querySelector("[data-content='chat']").style.display = 'block';
     }
 }
