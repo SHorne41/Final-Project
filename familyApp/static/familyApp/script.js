7ffa3a5265b3dfc9949ed666aa6f31299d8dab3c
SHorne41 <stephane.horne@gmail.com>
2020-10-27 17:01:27 -0400

    JS template added to each 'view'

--- familyApp/static/familyApp/script.js
+++ familyApp/static/familyApp/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    //Add event listeners for navigation pane buttons
+    //Add event listeners for buttons in navigation menu
     document.querySelector('#profile_tab').addEventListener('click', () => load_view('profile'));
     document.querySelector('#calendar_view').addEventListener('click', () => load_view('calendar'));
     document.querySelector('#chores_view').addEventListener('click', () => load_view('chores'));
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#sync_feature').addEventListener('click', () => sync_calendar());
 
     //Load the calendar view by default
-    load_view('profile');
+    load_view('calendar');
 
 });
 
@@ -24,7 +24,7 @@ function load_view(view){
         profileDiv.style.display = 'block';
         profileDiv.innerHTML = '';
         let success = document.createElement('p');
-        success.innerHTML = "Success bitch";
+        success.innerHTML = "Viewing the profile page.";
         profileDiv.appendChild(success);
 
     } else if (view === 'calendar'){
@@ -32,11 +32,27 @@ function load_view(view){
         calendarDiv.style.display = 'block';
         calendarDiv.innerHTML = '';
         let success = document.createElement('p');
-        success.innerHTML = "Calendar Success bitch";
+        success.innerHTML = "Currently viewing the calendar.";
         calendarDiv.appendChild(success);
+
     } else if (view === 'chores'){
-        document.querySelector("[data-content='chores']").style.display = 'block';
+        //Select the chores container, set its display, and erase any previous content
+        let choresDiv = document.querySelector("[data-content='chores']")
+        choresDiv.style.display = 'block';
+        choresDiv.innerHTML = '';
+        //Create any and all elements to be displayed and append them to the container
+        let success = document.createElement('p');
+        success.innerHTML = "Here's where all the chores will be displayed.";
+        choresDiv.appendChild(success);
+
     } else if (view === 'chat'){
-        document.querySelector("[data-content='chat']").style.display = 'block';
+        //Select the chat container, set its display, and erase any previous content
+        let chatDiv = document.querySelector("[data-content='chat']")
+        chatDiv.style.display = 'block';
+        chatDiv.innerHTML = '';
+        //Create any and all elements to be displayed, then append them to the container
+        let success = document.createElement('p');
+        success.innerHTML = "This is where the group chat can be viewed.";
+        chatDiv.appendChild(success);
     }
 }
