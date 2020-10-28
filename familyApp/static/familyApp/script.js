ee132645ebecbb23be913002d8c8003687172bed
SHorne41 <stephane.horne@gmail.com>
2020-10-28 11:35:19 -0400

    Basic chat elements created using JS

--- familyApp/static/familyApp/script.js
+++ familyApp/static/familyApp/script.js
@@ -50,9 +50,47 @@ function load_view(view){
         let chatDiv = document.querySelector("[data-content='chat']")
         chatDiv.style.display = 'block';
         chatDiv.innerHTML = '';
-        //Create any and all elements to be displayed, then append them to the container
-        let success = document.createElement('p');
-        success.innerHTML = "This is where the group chat can be viewed.";
-        chatDiv.appendChild(success);
+
+        /*
+        Create any and all elements to be displayed, then append them to the container
+        */
+        //chatContainer ("row div"; seperates chat container from message container)
+        let chatContainer = document.createElement('div');
+        chatContainer.className = "row h-75";
+        chatContainer.id="chatContainer";
+
+        //chatArea ("col div"; contains all chat elements)
+        let chatArea = document.createElement('div');
+        chatArea.class = "col-8";
+
+        //Messages to be displayed in the chatArea
+        let chatMessage = document.createElement('p');
+        chatMessage.innerHTML = "All the messages";
+
+        //messageContainer ("row div"; seperates message container from chat container)
+        let messageContainer = document.createElement('div');
+        messageContainer.className = "row h-25";
+        messageContainer.id = "messageContainer";
+
+        //messageArea ("col div"; where the messages will be typed)
+        let message = document.createElement('p');
+        message.innerHTML = "Where all the messages will be typed";
+
+        //sendArea ("col div"; where the 'Send' button will be located)
+
+        /*
+        Append items to corresponding containers
+        */
+        //Chat elements
+        chatArea.appendChild(chatMessage);
+        chatContainer.appendChild(chatArea);
+
+        //Message elements
+        messageContainer.appendChild(message);
+
+        //chatDiv elements
+        chatDiv.appendChild(chatContainer);
+        chatDiv.appendChild(messageContainer);
+
     }
 }
